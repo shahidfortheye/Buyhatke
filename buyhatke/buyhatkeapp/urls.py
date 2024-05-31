ed9070d3c09bca009bcb8d56d82560e6e5b993e4
shahidfortheye <109901959+shahidfortheye@users.noreply.github.com>
2024-05-31 15:03:01 +0530

    Update urls.py

--- buyhatke/buyhatkeapp/urls.py
+++ buyhatke/buyhatkeapp/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path(r'parse-url/', views.ParseUrl.as_view()),
     path(r'add-email/', views.AddEmailNotification.as_view()),
     path(r'add-bulk-products/', views.AddProductsFromSheet.as_view()),
+    path(r'update-product/', views.UpdateProduct.as_view()),
+    path(r'get-all-urls/', views.GetAllUrls.as_view()),
+    path(r'get-product-chart/', views.GetProductChart.as_view()),
+
+
     ]
