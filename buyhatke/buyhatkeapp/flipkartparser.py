4d29e89670b60fe3247b23515bb64c49354e8cb9
shahidfortheye <109901959+shahidfortheye@users.noreply.github.com>
2024-06-02 20:48:41 +0530

    Update flipkartparser.py

--- buyhatke/buyhatkeapp/flipkartparser.py
+++ buyhatke/buyhatkeapp/flipkartparser.py
@@ -65,7 +65,7 @@ class FlipkartScrapper:
         
     def get_driver(self):
         options = Options()
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
