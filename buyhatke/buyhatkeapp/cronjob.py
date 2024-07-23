5976a34448ba5c23151f0be6b2b1db4c87d24912
shahidfortheye <shahid.dar@fortheye.co>
2024-07-23 23:57:38 +0530

    add amazon parser

--- buyhatke/buyhatkeapp/cronjob.py
+++ buyhatke/buyhatkeapp/cronjob.py
@@ -32,9 +32,9 @@ def scrapper():
 
         elif obj.get("platform") == "amazon":
             obj1 = AmazonParser()
-
-            result = obj1.search_product(obj)
-            if not result.get("title"):
+            try:
+                result = obj1.search_product(obj)
+            except:
                 obj1.fetch_captcha_page()
                 result = obj1.search_product(obj)
         else:
